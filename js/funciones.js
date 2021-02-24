6e281fe85d1321a3b1f7e7957d362fdc46cd1215
Bernat Jorquera <bernatjv19@gmail.com>
2021-02-24 20:19:37 +0100

    Funcion 2

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,7 @@
 const equiposMayoresEdad = (equipos, edad) => {
   return equipos.filter(elemento => elemento.asignado.empleado.edad > edad);
 }
+
+const equiposProvincia = (equipos, provincia) => {
+  return equipos.filter(elemento => elemento.asignado.provincia.toLowerCase() === provincia.toLowerCase());
+}
